3138e28273cad1f108f06b0b6598043114f8d6f8
rudy11062003 <rudy11062003@gmail.com>
2025-01-31 16:23:14 +0000

    fl

---
+++ .next/static/HA3zVLSzfJHodEnWQUN_p/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/pages/index-7c3807e14f846000.js"],"/_error":["static/chunks/pages/_error-7a92967bea80186d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
